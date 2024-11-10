d981b6ccab5417ca3a4ed50ab2802edf9d93876c
huyle <huyle210504@gmail.com>
2024-11-10 21:12:59 +0700

    m

--- chapter-1/fahrenheit_toCelsius.c
+++ chapter-1/fahrenheit_toCelsius.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 main(){
-    int fahr, celsius;
+    float fahr, celsius;
     int maxFahrenheit, step;
 
     maxFahrenheit = 300;
     fahr = 0;
     while (fahr <= maxFahrenheit) {
         celsius = 5 * (fahr - 30) / 9;
-        printf("%d\n", celsius);
+        printf("%3.0f %3.1f\n", fahr, celsius);
 
         fahr = fahr + 20;
     }
